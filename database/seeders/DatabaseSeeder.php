f40038fd537adf85f8d0b7e729d0700323688f8e
Felipe <adfelipealmeida@gmail.com>
2024-08-07 14:34:41 -0300

    HEY-10: arrumados arquivos para passar no Pint

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,18 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
-use App\Models\Question;
-use App\Models\User;
+use App\Models\{Question, User};
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Test User',
+            'name'  => 'Test User',
             'email' => 'test@example.com',
         ]);
 
-        Question::factory()->count(10)->create();
+        Question::factory()->count(100)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     if (app()->isLocal()) {
         auth()->loginUsingId(1); // loga com o usuário de ID 1
+
         return to_route('dashboard');
     }
 
