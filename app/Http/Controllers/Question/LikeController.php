dc6f62638be9fddf890b6da0e37269aa157f4dd4
Felipe <adfelipealmeida@gmail.com>
2024-08-19 16:18:58 -0300

    HEY-16: mostrando para a IDE que o auth()->user() é a model User

--- app/Http/Controllers/Question/LikeController.php
+++ app/Http/Controllers/Question/LikeController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
-use App\Models\{Question};
+use App\Models\{Question, User};
 use Illuminate\Http\{RedirectResponse};
 
 class LikeController extends Controller
 {
     public function __invoke(Question $question): RedirectResponse
     {
+        /** @var User $user */
+        $user = auth()->user();
         // Mais Fácil a leitura: Usuário autenticado deu like na pergunta
-        auth()
-            ->user()
-            ->like($question);
+        $user->like($question);
 
         return back(); // Retorna para a página de onde saiu
     }
