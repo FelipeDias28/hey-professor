8f8275817b56e0906ed9993b1d548dba6219060f
Felipe <adfelipealmeida@gmail.com>
2024-09-13 17:59:33 -0300

    HEY-16: adicionado retorna da func invoke

--- app/Http/Controllers/Question/UnlikeController.php
+++ app/Http/Controllers/Question/UnlikeController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
 use App\Models\Question;
+use Illuminate\Http\RedirectResponse;
 
 class UnlikeController extends Controller
 {
-    public function __invoke(Question $question)
+    public function __invoke(Question $question): RedirectResponse
     {
         user()->unlike($question);
 
